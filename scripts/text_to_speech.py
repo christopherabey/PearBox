2a10c12a5efdea0c12189883a5e42cbabeb35e6e
Kushion32 <kushaldeepm@gmail.com>
2021-11-06 17:33:44 -0400

    Update to order

--- scripts/text_to_speech.py
+++ scripts/text_to_speech.py
@@ -1,8 +1,11 @@
-import pyttsx3 as tts
-engine = tts.init()
+import pyttsx3 as tts, time
+#engine = tts.init()
 def text_to_speech(text):
+    engine = tts.init()
+    engine.setProperty('rate', 125)
     engine.say(text)
     engine.runAndWait() 
+    engine.stop()
 
 text_to_speech('word')
-engine.stop()
+#engine.stop()
