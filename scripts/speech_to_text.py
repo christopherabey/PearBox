b1056b77ef61f6baf41235a35b64c33c6234cdf1
chrisabey <cabey@uwaterloo.ca>
2021-11-06 16:51:07 -0400

    made a function

--- scripts/speech_to_text.py
+++ scripts/speech_to_text.py
@@ -1,29 +1,31 @@
 import speech_recognition as sr
 from os import path
 
-#obtain path to "audio1.wav" in the same folder as this script
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "../audio/convince_me_more.wav")
 
-#use the audio file as the source
-r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-    audio = r.record(source)
+def speechRecognition():
+    #obtain path to "audio1.wav" in the same folder as this script
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "../audio/kushal.wav")
 
-## recognize speech using Sphinx
-# try:
-#     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-# except sr.UnknownValueError:
-#     print("Sphinx could not understand audio")
-# except sr.RequestError as e:
-#     print("Sphinx error; {0}".format(e))
+    #use the audio file as the source
+    r = sr.Recognizer()
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source)
 
-#recognize speech using Google
-try:
-    # for testing purposes, we're just using the default API key
+    ## recognize speech using Sphinx
+    # try:
+    #     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    # except sr.UnknownValueError:
+    #     print("Sphinx could not understand audio")
+    # except sr.RequestError as e:
+    #     print("Sphinx error; {0}".format(e))
+
+    #recognize speech using Google
+    try:
+        # for testing purposes, we're just using the default API key
     
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
