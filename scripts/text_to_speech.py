fe7a0eb3622aa63a611c2cf11730bbc3878f8901
Kushion32 <kushaldeepm@gmail.com>
2021-11-13 12:57:30 -0500

    Removed function calling in file

--- scripts/text_to_speech.py
+++ scripts/text_to_speech.py
@@ -1,11 +1,9 @@
 import pyttsx3 as tts, time
-#engine = tts.init()
 def text_to_speech(text):
     engine = tts.init()
-    engine.setProperty('rate', 125)
+    engine.setProperty('rate', 100)
     engine.say(text)
     engine.runAndWait() 
     engine.stop()
 
-text_to_speech('word')
-#engine.stop()
+
