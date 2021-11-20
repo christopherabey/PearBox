4ba11d8c37d932f5d1a5c96ac6a4f521a47199b1
Chris Abey <cabey@uwaterloo.ca>
2021-11-20 15:38:06 -0500

    text-to-speech is confirmed to work with files now.

--- scripts/text_to_speech.py
+++ scripts/text_to_speech.py
@@ -11,13 +11,10 @@ def tts_from_file():
 
     #takes the text from text.txt, reads it, translates it, and saves it back to the file
     try:
-        with open('text.txt', 'r') as file:
+        with open('../text.txt', 'r') as file:
             text = file.read()
             speak(text)
     
     #if there is a problem finding the file we're reading from, tells the user that.
     except FileNotFoundError:
         print('There was an error with locating the input file.  Please try again later.')
-
-#to test once someone whose computer runs this downloads it
-#tts_from_file()
