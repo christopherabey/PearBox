98562caddc3ba390ca0047f7d11e946b8d8cb2a9
Avril Chen <avril.chen@uwaterloo.ca>
2021-11-25 19:51:05 +0000

    Update ocr.py

--- scripts/ocr.py
+++ scripts/ocr.py
@@ -43,11 +43,6 @@ def OCR():
     os.remove(filename)
     print(text)
 
-    #show the output images
-    cv2.imshow("Image", image)
-    cv2.imshow("Output", gray)
-    cv2.waitKey(0)
-
 def ocr(path):
     image = cv2.imread(path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
